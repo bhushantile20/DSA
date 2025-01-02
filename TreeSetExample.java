ec81852a3d4aa3bb48cee256d19218ec145010d1
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2025-01-03 00:00:05 +0530

    Add files via upload

---
+++ TreeSetExample.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class TreeSetExample {
+    public static void main(String argsp[])
+
+    {
+        TreeSet ts = new TreeSet();
+        ts.add("Bhushan");
+        System.out.println("THe element is " + ts);
+    }
+}
