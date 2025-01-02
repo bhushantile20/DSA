f387e4164bf15211d77d5d9167adadebd358e297
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2025-01-02 23:57:26 +0530

    Add files via upload

---
+++ HashSetExample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class HashSetExample {
+    public static void main(String args[])
+
+{
+     HashSet hs=new HashSet();
+     hs.add("D");
+     hs.add("A");
+     hs.add("C");
+     hs.add("B");
+     hs.add("E");
+
+     System.out.println("The elements available in the hash set are" +hs);
+}
+}
